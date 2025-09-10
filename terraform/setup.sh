522bfbf9b71fafdba273546d5b8cbcb2c5fc8a5e
Ubuntu <ubuntu@ip-172-31-17-185.ec2.internal>
2025-09-10 06:40:34 +0000

    ping

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -1,33 +1,4 @@
-#!/bin/bash
-
-# === Install GitHub Runner ===
-sudo apt-get update -y
-sudo apt-get install -y curl tar jq
-
-# Create runner directory
-sudo mkdir -p /home/ubuntu/actions-runner
-cd /home/ubuntu/actions-runner
-
-# Download and extract runner
-curl -o actions-runner.tar.gz -L https://github.com/actions/runner/releases/download/v2.328.0/actions-runner-linux-x64-2.328.0.tar.gz
-tar xzf ./actions-runner.tar.gz
-rm actions-runner.tar.gz
-
-echo "Runner token received: ${runner_token}" >> /var/log/user-data.log
-
-sudo chown -R ubuntu:ubuntu /home/ubuntu/actions-runner
-
-# Configure runner as ubuntu user
-sudo -u ubuntu ./config.sh --unattended \
-  --url https://github.com/awsdeployer \
-  --token "${runner_token}" \
-  --name org_runner \
-  --work "_work" >> /var/log/runner-config.log 2>&1
-
-
 sudo hostnamectl set-hostname master
-sudo ./svc.sh install
-sudo ./svc.sh start
 # ===== Install Docker =====
 sudo apt remove -y docker docker-engine docker.io containerd runc || true
 sudo apt install -y docker.io
@@ -98,3 +69,30 @@ NODE_IP=$(kubectl get nodes -o jsonpath="{.items[0].status.addresses[0].address}
 echo "Prometheus URL: http://$NODE_IP:32000"
 echo "Grafana URL: http://$NODE_IP:32001"
 
+# === Install GitHub Runner ===
+sudo apt-get update -y
+sudo apt-get install -y curl tar jq
+
+# Create runner directory
+sudo mkdir -p /home/ubuntu/actions-runner
+cd /home/ubuntu/actions-runner
+
+# Download and extract runner
+curl -o actions-runner.tar.gz -L https://github.com/actions/runner/releases/download/v2.328.0/actions-runner-linux-x64-2.328.0.tar.gz
+tar xzf ./actions-runner.tar.gz
+rm actions-runner.tar.gz
+
+echo "Runner token received: ${runner_token}" >> /var/log/user-data.log
+
+sudo chown -R ubuntu:ubuntu /home/ubuntu/actions-runner
+
+# Configure runner as ubuntu user
+sudo -u ubuntu ./config.sh --unattended \
+  --url https://github.com/awsdeployer \
+  --token "${runner_token}" \
+  --name org_runner \
+  --work "_work" >> /var/log/runner-config.log 2>&1
+
+# Install and start service
+sudo ./svc.sh install
+sudo ./svc.sh start
