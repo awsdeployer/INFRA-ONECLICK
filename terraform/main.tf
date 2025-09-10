a852e5178d40260a90e8606df88f199241014d71
Ubuntu <ubuntu@ip-172-31-17-185.ec2.internal>
2025-09-10 11:21:52 +0000

    completed

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,19 @@
 terraform {
   required_version = ">= 1.5.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+  }
+
+  backend "s3" {
+    bucket         = "awsdeployer"           # Your S3 bucket name
+    key            = "terraform.tfstate"     # Path inside bucket
+    region         = var.aws_region          # Use the same region as provider
+    dynamodb_table = "terraform-locks"       # DynamoDB table for locking
+    encrypt        = true                    # Encrypt the state file
   }
 }
 
