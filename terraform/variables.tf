95a1f649cddde0bdb11bab1305b0862a567ab69e
ashwanth <115695893+ashwanth01-pixel@users.noreply.github.com>
2025-09-10 11:12:56 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,22 +1,23 @@
-variable "ami_id" {
-  description = "AMI ID"
+variable "aws_region" {
+  description = "AWS region to deploy into"
   type        = string
-  default     = "ami-04f59c565deeb2199"
+  default     = "us-east-1"
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.large"
+  default     = "t2.medium"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the instance"
+  type        = string
+  default     = "ami-04f59c565deeb2199"
 }
 
 variable "runner_token" {
-  description = "GitHub runner token"
+  description = "GitHub token for runner registration"
   type        = string
-}
-
-variable "aws_region" {
-  description = "AWS region to deploy into"
-  type        = string
-  default     = "us-east-1"
+  sensitive   = true
 }
